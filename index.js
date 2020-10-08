b93b8c1c0c5cc27da2b52651aba2e29e21bcf11a
mrchoc <mrchoc@github.com>
2020-10-08 22:34:43 +0800

    removed telegram module

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const { Composer } = require('micro-bot');
 const bot = new Composer;
 const Telegram = require('telegraf/telegram')
 
-const BOT_TOKEN = process.env.BOT_TOKEN;
+// const BOT_TOKEN = process.env.BOT_TOKEN;
 // const bot = new Telegraf(BOT_TOKEN);
-const telegram = new Telegram(BOT_TOKEN);
+// const telegram = new Telegram(BOT_TOKEN);
 var started = false;
 var fromChatId = '';
 var chatId = '';
@@ -19,26 +19,26 @@ async function getChatName(id) {
 bot.command('sourcechat', async (ctx) => {
 
   fromChatId = ctx.message.chat.id;
-  ctx.reply(`*${await getChatName(fromChatId)}* chat will be the source`);
+  ctx.reply(`This chat will be the source`);
 
 });
 
 bot.command('targetchat', async (ctx) => {
 
   chatId = ctx.message.chat.id;
-  ctx.reply(`*${await getChatName(chatId)}* chat will be the target`);
+  ctx.reply(`This chat will be the target`);
 
 });
 
-bot.command('startforwarding', async (ctx) => {
-
-  if (!fromChatId) return ctx.reply('/sourcechat in chat to messages from before you /startforwarding');
-  if (!chatId) return ctx.reply('/targetchat in chat to messages to before you /startforwarding');
-
-  started = true;
-  ctx.reply(`Messages from *${await getChatName(fromChatId)}* will now be forwarded to *${await getChatName(chatId)}*`)
-
-});
+// bot.command('startforwarding', async (ctx) => {
+//
+//   if (!fromChatId) return ctx.reply('/sourcechat in chat to messages from before you /startforwarding');
+//   if (!chatId) return ctx.reply('/targetchat in chat to messages to before you /startforwarding');
+//
+//   started = true;
+//   ctx.reply(`Messages from *${await getChatName(fromChatId)}* will now be forwarded to *${await getChatName(chatId)}*`)
+//
+// });
 
 bot.help((ctx) => {
   ctx.reply(`/sourcechat@msgdeflect_bot to set chat as source\n/targetchat@msgdeflect_bot to set chat as target\n/startforwarding@msgdeflect_bot to start forwarding messages sent in source chat to target chat\n/restart@msgdeflect_bot to reset target and source chats`)
