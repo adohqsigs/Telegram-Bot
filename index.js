7114e726a38035a900b5292de79306137001bfdc
mrchoc <mrchoc@github.com>
2020-10-08 23:25:36 +0800

    added telegram module

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // const { Telegraf } = require('telegraf');
 const { Composer } = require('micro-bot');
 const bot = new Composer;
-// const Telegram = require('telegraf/telegram')
+const Telegram = require('telegraf/telegram')
 
-// const BOT_TOKEN = process.env.BOT_TOKEN;
+const BOT_TOKEN = process.env.BOT_TOKEN;
 // const bot = new Telegraf(BOT_TOKEN);
-// const telegram = new Telegram(BOT_TOKEN);
+const telegram = new Telegram(BOT_TOKEN);
 var started = false;
 var fromChatId = '';
 var chatId = '';
 
-// async function getChatName(id) {
-//   const chat = await telegram.getChat(id);
-//   return (chat.title)
-// };
+async function getChatName(id) {
+  const chat = await telegram.getChat(id);
+  return (chat.title)
+};
 
 
 bot.command('sourcechat', async (ctx) => {
@@ -30,15 +30,15 @@ bot.command('targetchat', async (ctx) => {
 
 });
 
-// bot.command('startforwarding', async (ctx) => {
-//
-//   if (!fromChatId) return ctx.reply('/sourcechat in chat to messages from before you /startforwarding');
-//   if (!chatId) return ctx.reply('/targetchat in chat to messages to before you /startforwarding');
-//
-//   started = true;
-//   ctx.reply(`Messages from *${await getChatName(fromChatId)}* will now be forwarded to *${await getChatName(chatId)}*`)
-//
-// });
+bot.command('startforwarding', async (ctx) => {
+
+  if (!fromChatId) return ctx.reply('/sourcechat in chat to messages from before you /startforwarding');
+  if (!chatId) return ctx.reply('/targetchat in chat to messages to before you /startforwarding');
+
+  started = true;
+  ctx.reply(`Messages from *${await getChatName(fromChatId)}* will now be forwarded to *${await getChatName(chatId)}*`)
+
+});
 
 bot.help((ctx) => {
   ctx.reply(`/sourcechat@msgdeflect_bot to set chat as source\n/targetchat@msgdeflect_bot to set chat as target\n/startforwarding@msgdeflect_bot to start forwarding messages sent in source chat to target chat\n/restart@msgdeflect_bot to reset target and source chats`)
@@ -51,17 +51,17 @@ bot.command('restart', (ctx) => {
   ctx.reply('sourcechat and targetchat have been reset')
 });
 
-// bot.on('message', (ctx) => {
-//   if (ctx.message.chat.id === fromChatId && chatId) {
-//     if (started) {
-//       var message = ctx.message.text;
-//       var messageId = ctx.message.message_id;
-//       telegram.forwardMessage(chatId, fromChatId, messageId)
-//     }
-//     else {
-//       ctx.reply('This message was not forwarded. /startforwarding to start forwarding')
-//     }
-//   }
-// });
+bot.on('message', (ctx) => {
+  if (ctx.message.chat.id === fromChatId && chatId) {
+    if (started) {
+      var message = ctx.message.text;
+      var messageId = ctx.message.message_id;
+      telegram.forwardMessage(chatId, fromChatId, messageId)
+    }
+    else {
+      ctx.reply('This message was not forwarded. /startforwarding to start forwarding')
+    }
+  }
+});
 
 module.exports = bot;
