e7ba4ab15fd83d99e166b7e40cdb821e191290ae
mrchoc <mrchoc@github.com>
2020-10-10 00:16:46 +0800

    added channel messaging

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { Composer } = require('micro-bot');
 const bot = new Composer;
 const Telegram = require('telegraf/telegram')
 
-
-// const bot = new Telegraf(BOT_TOKEN);
+// const telegram = new Telegram('1364016845:AAEIYZHp7SD8A2BvDHl5m3r8G-I_QPqtBDA');
+// const bot = new Telegraf('1364016845:AAEIYZHp7SD8A2BvDHl5m3r8G-I_QPqtBDA');
 
 var started = false;
 var chatId = '';
@@ -12,21 +12,29 @@ var chatId = '';
 
 
 bot.start((ctx) => {
-  ctx.channelPost(`/startforwarding to begin forwarding messages from sms source\n/stop to stop forwarding sms messages\nMake sure bot is added to chat or channel for this to work`);
+  chatId = ctx.channelPost.chat.id;
+  console.log(chatId);
+  telegram.sendMessage(chatId, `/startforwarding to begin forwarding messages from sms source\n/stop to stop forwarding sms messages\n*Make sure bot is added to chat or channel for this to work`);
 });
 
 bot.command('startforwarding', (ctx) => {
-  chatId = ctx.message.chat.id;
+  chatId = ctx.channelPost.chat.id;
+  console.log(chatId);
   started = true;
-  ctx.channelPost('bot will now forward sms messages')
+  ctx.reply(chatId, 'bot will now forward sms messages')
 });
 
 
 bot.command('stop', (ctx) => {
-  chatId = '';
+  chatId = ctx.channelPost.chat.id;
+  console.log(chatId);
   started = false;
-  ctx.channelPost('bot has stop forwarding sms messages')
+  ctx.reply(chatId, 'bot has stop forwarding sms messages')
 });
 
+bot.command('send', (ctx) => {
+  telegram.sendMessage(process.env.CHANNEL_ID, 'message')
+});
 
 module.exports = bot;
+// bot.launch();
