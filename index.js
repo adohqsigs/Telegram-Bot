01c75525e17cee4aca4e1993b31bfac52d7db665
mrchoc <mrchoc@github.com>
2020-10-10 23:15:24 +0800

    commented micro bot

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-// const { Telegraf } = require('telegraf');
-const { Composer } = require('micro-bot');
-const bot = new Composer;
-const Telegram = require('telegraf/telegram')
+const { Telegraf } = require('telegraf');
+// const { Composer } = require('micro-bot');
+// const bot = new Composer;
+const Telegram = require('telegraf/telegram');
 const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser');
 
 
 const telegram = new Telegram(process.env.BOT_TOKEN);
-// const bot = new Telegraf('1364016845:AAEIYZHp7SD8A2BvDHl5m3r8G-I_QPqtBDA');
+const bot = new Telegraf(process.env.BOT_TOKEN);
 
 const app = express();
 
@@ -38,5 +38,5 @@ http.createServer(app).listen(port, () => {
 
 
 
-module.exports = bot;
-// bot.launch();
+// module.exports = bot;
+bot.launch();
