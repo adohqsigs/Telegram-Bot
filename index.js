4edb87f7696f70287f2f7dae22023332a10f9a33
mrchoc <mrchoc@github.com>
2020-10-10 10:38:24 +0800

    changed port listening

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
 
+const port = process.env.PORT || 80
 const telegram = new Telegram(process.env.BOT_TOKEN);
 // const bot = new Telegraf('1364016845:AAEIYZHp7SD8A2BvDHl5m3r8G-I_QPqtBDA');
 
@@ -14,7 +15,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('morning-scrubland-32486.herokuapp.com/sms', (req, res) => {
+app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
 
   telegram.sendMessage(process.env.CHANNEL_ID, req.body.Body)
@@ -24,10 +25,11 @@ app.post('morning-scrubland-32486.herokuapp.com/sms', (req, res) => {
   res.end(twiml.toString());
 });
 
-http.createServer(app).listen(3000, () => {
-  console.log('Express server listening on port 3000');
-});
+// http.createServer(app).listen(3000, () => {
+//   console.log('Express server listening on port 3000');
+// });
 
+app.listen(port, console.log(`listening on port ${port}`));
 
 
 
