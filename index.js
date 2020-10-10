314fbab73274f6d71735a918992e906adb6e48b3
mrchoc <mrchoc@github.com>
2020-10-10 13:59:53 +0800

    debugging

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
 
-const port = 3000
+
 const telegram = new Telegram(process.env.BOT_TOKEN);
 // const bot = new Telegraf('1364016845:AAEIYZHp7SD8A2BvDHl5m3r8G-I_QPqtBDA');
 
@@ -15,22 +15,18 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-  res.send('hello');
-})
-
 app.post('/sms', (req, res) => {
   const twiml = new MessagingResponse();
 
   telegram.sendMessage(process.env.CHANNEL_ID, req.body.Body)
 
-  res.send('message received')
-  res.writeHead(200, {'Content-Type': 'text/xml'});
+
+  res.send(req.body.Body);
   res.end(twiml.toString());
 });
 
-http.createServer(app).listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
+http.createServer(app).listen(3000, () => {
+  console.log(`Express server listening on port 3000`);
 });
 
 
