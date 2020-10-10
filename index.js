4e9b89af4be26793867f4ffdc4624942bd43021c
mrchoc <mrchoc@github.com>
2020-10-10 14:35:42 +0800

    added port listen

--- index.js
+++ index.js
@@ -26,6 +26,10 @@ app.post('/sms', (req, res) => {
   res.end(twiml.toString());
 });
 
+const port = process.env.PORT || 3000
+http.createServer(app).listen(port, () => {
+  console.log(`Express server listening on port ${port}`);
+});
 
 
 
