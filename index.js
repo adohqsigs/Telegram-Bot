b713814e0205e48a9f136010cbfae175d15143b1
mrchoc <mrchoc@github.com>
2020-10-10 11:56:05 +0800

    debugging

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 3000
+const port = 3000
 const telegram = new Telegram(process.env.BOT_TOKEN);
 // const bot = new Telegraf('1364016845:AAEIYZHp7SD8A2BvDHl5m3r8G-I_QPqtBDA');
 
