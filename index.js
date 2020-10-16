eb6b81d0fb212ea5ef27855f6cd679e573de8f2b
mrchoc <mrchoc@github.com>
2020-10-16 21:41:10 +0800

    removed micro bot

--- index.js
+++ index.js
@@ -1,22 +1,20 @@
-// const { Telegraf } = require('telegraf');
-const { Composer } = require('micro-bot');
-const bot = new Composer;
+const { Telegraf } = require('telegraf');
+// const { Composer } = require('micro-bot');
+// const bot = new Composer;
 const Telegram = require('telegraf/telegram');
-const http = require('http');
 const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
-
 const bodyParser = require('body-parser');
-
-
-const telegram = new Telegram(process.env.BOT_TOKEN);
-// const bot = new Telegraf(process.env.BOT_TOKEN);
-
 const app = express();
 
+const telegram = new Telegram(process.env.BOT_TOKEN);
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.telegram.setWebhook(`${process.env.BOT_DOMAIN}/bot${process.env.BOT_TOKEN}`)
+app.use(bot.webhookCallback(`/bot${process.env.BOT_TOKEN}`));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/sms', (req, res) => {
+  if (req.body.From !== process.env.SOURCE_NO) return res.status(401).send(`User ${req.body.From} not authorised to send messages`);
   console.log(`${req.body.From} sent an sms to the bot`);
   const twiml = new MessagingResponse();
 
@@ -26,17 +24,14 @@ app.post('/sms', (req, res) => {
     .catch((err) => console.log(err));
 
 
-  res.writeHead(200, {'Content-Type': 'text/xml', });
-  res.end(twiml.toString());
+  res.send('received');
 
 });
 
-const port = process.env.PORT || 3000
-http.createServer(app).listen(port, () => {
-  console.log(`Express server listening on port ${port}`);
-});
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Express server listening on port ${port}`));
 
 
 
-module.exports = bot;
-// bot.launch();
+// module.exports = bot;
+bot.launch();
